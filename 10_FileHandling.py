7b3a470e893d90996676258b8a04dab8f1604ce4
Ashutosh Giri <ashugoswami339@gmail.com>
2026-03-26 20:34:54 +0530

    file handeling

--- 10_FileHandling.py
+++ 10_FileHandling.py
@@ -1,6 +1,31 @@
-f=open("02_practice_set1.py")
+f = open("trash.txt")
+
 data = f.read()
 print(data)
+
+f.seek(0)   # move pointer back to start
+
+data = f.readlines()
+print(data)
+
 f.close()
 
-# wrte on file
+
+#open a file and replace a word from the file
+with open("trash.txt","r") as f:
+    data= f.read()
+
+new_data = data.replace("java","python")
+print(new_data)
+
+with open("trash.txt","w") as f:
+    f.write(new_data)
+
+#find a word in a file 
+word = input()
+with open("trash.txt","r") as f:
+    data= f.read()
+    if(data.find(word)!=-1):
+        print("Found")
+    else:
+        print("Not Found")
