0ec82009351a416049e434778f03a403ab30033b
Ashutosh Giri <ashugoswami339@gmail.com>
2026-03-26 18:08:04 +0530

    practice

---
+++ practice/ForLoops.py
@@ -0,0 +1,23 @@
+i=0
+
+while i<100:
+    print(i)
+    i+=1
+
+while i>=1:
+     print(i)
+     i-=1
+
+
+print("Enter the number which table you want to print")
+num = int(input())
+i=1
+while i<=10:
+    print(num,"*",i,"=",num*i)
+    i+=1
+i=1
+a=1
+while  a<100:
+    print(a)
+    a+=2**i+1
+    i+=1
